da9223382627ccb65c7d9f5b4c66099242cc947f
Anigko <anigko@yandex.ru>
2022-03-30 21:26:05 +0300

    amended for try-with-resources

--- src/main/java/ServerSocketApp.java
+++ src/main/java/ServerSocketApp.java
@@ -12,17 +12,18 @@ public class ServerSocketApp {
 
         while (true) {
 
-            ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-            Socket clientSocket = serverSocket.accept(); // ждем подключения
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            try (ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+                 Socket clientSocket = serverSocket.accept(); // ждем подключения
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            ) {
 
-            System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
+                out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
 
-            final String name = in.readLine();
+                final String name = in.readLine();
 
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-            serverSocket.close();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            }
         }
     }
 }
